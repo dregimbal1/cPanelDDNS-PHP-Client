37b3ca02c051c33c235f75496e4e8bf8439bceab
David Regimbal <regimbal93@gmail.com>
2016-05-02 01:29:09 -0400

    Update config.php

--- Core/config.php
+++ Core/config.php
@@ -6,28 +6,28 @@
  *
  * LICENSE: 
  *	
- * @package 		DDNSUpdater
- * @author 			David Regimbal <david@regimbal.me>
+ * @package	DDNSUpdater
+ * @author 	David Regimbal <david@regimbal.me>
  * @copyright 	2016 David Regimbal
- * @version 		0.0.1
- * @link				http://github.com/regimbal93
+ * @version 	0.0.1
+ * @link	http://github.com/regimbal93
  *
  */
 
 $config = array(
-    'cpanel'			=> array(
-	    'host'			=> '',							# cPanel host address
-			'port'			=> '2083',					# default
-			'account' 	=> '',							# same as your username most likely					
-	    'user'			=> '',							# cPanel username
-	    'password'	=> ''								# cPanel password
-    ),
-		'settings' => array(
-			'domain'	=> '',								# the record's domain.
-			'name'		=> '',								# subdomain if applicable
-			'ttl'			=> '',								# the record's time to live, in seconds.
-			'type'		=> 'A'								# 'A' Record
-		),
+   'cpanel' => array(
+	'host' 		=> '',		# cPanel host address
+	'port' 		=> '2083',	# default
+	'account' 	=> '',		# same as your username most likely					
+	'user'		=> '',		# cPanel username
+	'password'	=> ''		# cPanel password
+   ),
+   'settings' => array(
+	'domain'	=> '',		# the record's domain.
+	'name'		=> '',		# subdomain if applicable
+	'ttl'		=> '',		# the record's time to live, in seconds.
+	'type'		=> 'A'		# 'A' Record
+   ),
 );
 
 // Set to true to view log output
